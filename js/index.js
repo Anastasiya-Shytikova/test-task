a231bc7096749dcde374cbdcaebe9f79aad737af
Anastasiia <a0930578458@gmail.com>
2022-06-01 19:59:56 +0300

    button problems

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-    $('#my_table').DataTable();
+    $('#my_table').DataTable({
+        buttons: [{
+            text: 'Apply',
+            className: 'applyButton',
+            action: function(e, dt, node, config) {
+                alert('Activated!');
+            }
+        }]
+    })
 });
+
+// $(document).ready(function() {
+//     $('#my_table_wrapper').DataTable({
+//         dom: '<"toolbar">frtip',
+//         fnInitComplete: function() {
+//             $('div.toolbar').html('<b>Custom tool bar! Text/images etc.</b>');
+//         }
+//     });
+// });
