c127ea1557b7af09d9723564de508a67ca0bed1a
Anastasiia <a0930578458@gmail.com>
2022-06-03 17:13:10 +0300

    buttons addition

--- js/index.js
+++ js/index.js
@@ -1,66 +1,88 @@
-// $(document).ready(function() {
-//     var table = $('#my_table').DataTable({
-//         search: {
-//             return: true,
-//         },
-//         dom: 'Bfrtip',
-//         buttons: [{
-//             text: 'Apply',
-//             className: 'applyButton',
-//             action: function(e, dt, node, config) {
-//                 alert('Activated!');
-//             }
-//         }],
-//     });
-//     table.buttons().container()
-//         .appendTo($('.col-sm-6:eq(0)', table.table().container()));
-// });
-
-
 $(document).ready(function() {
-    var table = $('#my_table').DataTable({
+    $('#my_table').DataTable({
         dom: 'Bfrtip',
         buttons: [{
-            className: 'applyButton',
-            text: 'Button 1',
-            action: function(e, dt, node, config) {
-                alert('Button 1 clicked on');
-            }
-        }]
-    });
+                extend: 'collection',
+                text: 'Table control',
+                buttons: [{
+                        text: 'Toggle start date',
+                        action: function(e, dt, node, config) {
+                            dt.column(-2).visible(!dt.column(-2).visible());
+                        }
+                    },
+                    {
+                        text: 'Toggle salary',
+                        action: function(e, dt, node, config) {
+                            dt.column(-1).visible(!dt.column(-1).visible());
+                        }
 
-    //     new $.fn.dataTable.Buttons(table, {
-    //         buttons: [{
-    //                 text: 'Button 2',
-    //                 className: 'test',
-    //                 action: function(e, dt, node, conf) {
-    //                     alert('Button 2 clicked on');
-    //                 }
-    //             },
-    //             {
-    //                 text: 'Button 3',
-    //                 className: 'Button',
-    //                 action: function(e, dt, node, conf) {
-    //                     alert('Button 3 clicked on');
-    //                 }
-    //             }
-    //         ]
-    //     });
+                    },
+                    {
+                        text: 'Test',
+                        action: function(e, dt, node, config) {
+                            dt.column(-1).visible(!dt.column(-1).visible());
+                        }
 
-    //     table.buttons(1, null).container().appendTo(
-    //         table.table().container()
-    //     );
-    new $.fn.dataTable.Buttons(table, {
-        buttons: [{
-                text: 'High priority',
-                action: function() {}
+                    }
+                ]
             },
-            split: { text: 'Medium priority', action: function() {} },
-            { text: 'Low priority', action: function() {} },
+            {
+                text: 'Button 2',
+                className: 'Button',
+                action: function(e, dt, node, conf) {
+                    alert('Button 2 clicked on');
+                }
+            },
+            {
+                extend: 'collection',
+                text: 'Table control',
+                buttons: [{
+                        text: 'Toggle start date',
+                        action: function(e, dt, node, config) {
+                            dt.column(-2).visible(!dt.column(-2).visible());
+                        }
+                    },
+                    {
+                        text: 'Toggle salary',
+                        action: function(e, dt, node, config) {
+                            dt.column(-1).visible(!dt.column(-1).visible());
+                        }
+
+                    },
+                    {
+                        text: 'Test',
+                        action: function(e, dt, node, config) {
+                            dt.column(-1).visible(!dt.column(-1).visible());
+                        }
+
+                    }
+                ]
+            },
+            {
+                extend: 'collection',
+                text: 'Table control',
+                buttons: [{
+                        text: 'Toggle start date',
+                        action: function(e, dt, node, config) {
+                            dt.column(-2).visible(!dt.column(-2).visible());
+                        }
+                    },
+                    {
+                        text: 'Toggle salary',
+                        action: function(e, dt, node, config) {
+                            dt.column(-1).visible(!dt.column(-1).visible());
+                        }
+
+                    },
+                    {
+                        text: 'Test',
+                        action: function(e, dt, node, config) {
+                            dt.column(-1).visible(!dt.column(-1).visible());
+                        }
+
+                    }
+                ]
+            }
         ]
-    }]
+    });
 });
-
-
-
-// });
