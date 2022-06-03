9ecffcdf42b24ce5e287ed9ad638e9466d6f525b
Anastasiia <a0930578458@gmail.com>
2022-06-03 14:26:05 +0300

    button

--- js/index.js
+++ js/index.js
@@ -1,20 +1,66 @@
+// $(document).ready(function() {
+//     var table = $('#my_table').DataTable({
+//         search: {
+//             return: true,
+//         },
+//         dom: 'Bfrtip',
+//         buttons: [{
+//             text: 'Apply',
+//             className: 'applyButton',
+//             action: function(e, dt, node, config) {
+//                 alert('Activated!');
+//             }
+//         }],
+//     });
+//     table.buttons().container()
+//         .appendTo($('.col-sm-6:eq(0)', table.table().container()));
+// });
+
+
 $(document).ready(function() {
-    $('#my_table').DataTable({
+    var table = $('#my_table').DataTable({
+        dom: 'Bfrtip',
         buttons: [{
-            text: 'Apply',
             className: 'applyButton',
+            text: 'Button 1',
             action: function(e, dt, node, config) {
-                alert('Activated!');
+                alert('Button 1 clicked on');
             }
         }]
-    })
+    });
+
+    //     new $.fn.dataTable.Buttons(table, {
+    //         buttons: [{
+    //                 text: 'Button 2',
+    //                 className: 'test',
+    //                 action: function(e, dt, node, conf) {
+    //                     alert('Button 2 clicked on');
+    //                 }
+    //             },
+    //             {
+    //                 text: 'Button 3',
+    //                 className: 'Button',
+    //                 action: function(e, dt, node, conf) {
+    //                     alert('Button 3 clicked on');
+    //                 }
+    //             }
+    //         ]
+    //     });
+
+    //     table.buttons(1, null).container().appendTo(
+    //         table.table().container()
+    //     );
+    new $.fn.dataTable.Buttons(table, {
+        buttons: [{
+                text: 'High priority',
+                action: function() {}
+            },
+            split: { text: 'Medium priority', action: function() {} },
+            { text: 'Low priority', action: function() {} },
+        ]
+    }]
 });
 
-// $(document).ready(function() {
-//     $('#my_table_wrapper').DataTable({
-//         dom: '<"toolbar">frtip',
-//         fnInitComplete: function() {
-//             $('div.toolbar').html('<b>Custom tool bar! Text/images etc.</b>');
-//         }
-//     });
+
+
 // });
